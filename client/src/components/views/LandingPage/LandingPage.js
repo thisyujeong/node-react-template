633f206369ee1e31dcdbfc29442fddca1ac07aa3
thisyujeong <this.yujeong@gmail.com>
2021-11-01 22:14:00 +0900

    connect to Logout API

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -6,9 +6,23 @@ function LandingPage(props) {
     axios.get('/api/hello')
       .then(response => console.log(response.data));
   }, []);
+
+  const onClickHandler = (e) => {
+    e.preventDefault();
+    axios.get(`api/users/logout`)
+    .then(response => {
+      console.log(response.data);
+      if(response.data.success) {
+        props.history.push('/login');
+      } else {
+        alert('Failed to sign out.');
+      }
+    })
+  }
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', with: '100%', height: '100vh' }}>
       <h2>시작 페이지</h2>
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   );
 }
